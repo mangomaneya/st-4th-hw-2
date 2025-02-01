360fcabd9aebf0918c5d97effe142ba0ce8c1fab
lee kiri <mango.man.e.ya@gmail.com>
2025-02-02 01:35:49 +0900

    투두리스트 완료 및 삭제 기능 개선: 로그 메시지 수정, 완료 목록 핸들러 수정

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         { id: id, title: title.trim(), content: content.trim() },
       ];
     });
-    console.log(todoList);
+    console.log("todoList =>", todoList);
 
     resetInput();
   };
@@ -55,31 +55,43 @@ function App() {
   // 투두리스트 삭제
   const deleteHandler = (id) => {
     console.log(`삭제될 투두 id =>`, id);
-    const newTodoList = todoList.filter((todo) => {
+    const newDeletedTodoList = todoList.filter((todo) => {
       return todo.id !== id;
     });
-    setTodoList(newTodoList);
+
+    setTodoList(newDeletedTodoList);
   };
 
   // 완료 투두리스트 생성 - doneTodoList 스테이트 생성 / onclick이벤트
   const [doneTodoList, setDoneTodoList] = useState([]);
-  const doneTodoListHandler = (title) => {
-    console.log("title>", title);
-    const newTodoList = todoList.filter((todo) => {
-      return todo.title === title;
+  const doneTodoListHandler = (id) => {
+    // console.log("id>", id);
+    const newDoneTodoList = todoList.find((todo) => {
+      return todo.id === id;
     });
-    console.log("newTodoList", newTodoList);
+    console.log("newDoneTodoList =>", newDoneTodoList);
     setDoneTodoList((prev) => {
-      return [
-        ...prev,
-        { title: newTodoList.title, content: newTodoList.content },
-      ];
+      // console.log('prev =>', prev)
+      return [...prev, newDoneTodoList];
     });
+
+    //기존 투두리스트에서 현재 선택된 투두 삭제
+    deleteHandler(id);
+    console.log("todoList", todoList);
   };
 
-  // 완료 취소 동작 - 취소된 완료 투두리스트를, 기본 투두리스트에 추가
-  const workingTodoList = (todo) => {
-    console.log("todo", todo);
+  // 완료 취소 동작 - 취소된 완료 투두리스트를, 기본 투두리스트에 추가 / 완료리스트 재갱신
+  const workingTodoList = (id) => {
+    const newWorkingTodoList = doneTodoList.find((todo) => {
+      return todo.id === id;
+    });
+    setTodoList((prev) => {
+      return [...prev, newWorkingTodoList];
+    });
+    const newDoneTodoList = doneTodoList.filter((todo)=>{
+      return todo.id !== id;
+    })
+    setDoneTodoList(newDoneTodoList);
   };
 
   return (
@@ -126,7 +138,7 @@ function App() {
                 <p>{todo.title}</p>
                 <p>{todo.content}</p>
                 <button onClick={() => deleteHandler(todo.id)}>삭제</button>
-                <button onClick={() => workingTodoList(todo)}>취소</button>
+                <button onClick={() => workingTodoList(todo.id)}>취소</button>
               </li>
             );
           })}
